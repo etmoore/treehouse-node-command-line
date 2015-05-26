c86847381f4daf28be79f2a5cae8a24feed99902
Evan Moore <etmoore@gmail.com>
2015-05-25 21:11:43 -0600

    making request to Treehouse api

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 // Solution: Use Node.js to connect to Treehouse's API to get profile information
 // to print out
 
+var http = require('http');
+
+var username = 'etmoore';
 
 function printMessage(username, badgeCount, points){
   var message = username + " has " + badgeCount + " total badge(s) and " + points + " JavaScript points.";
@@ -9,6 +12,11 @@ function printMessage(username, badgeCount, points){
 }
 
 // Connect  to the API URL (http://teamtreehouse.com/username.json)
-// Read the data
-// Parse the data
-// Print the data
+
+var req = http.get('http://teamtreehouse.com/' + username + '.json', function(res){
+  console.log(res.statusCode);
+  // Read the data
+  // Parse the data
+  // Print the data
+
+});
