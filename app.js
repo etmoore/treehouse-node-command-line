22e3a2ec080767dc7168d3e788b8fa39c3dbd86b
Evan Moore <etmoore@gmail.com>
2015-05-25 21:46:36 -0600

    helpful error message

--- app.js
+++ app.js
@@ -6,28 +6,43 @@ var http = require('http');
 
 var username = 'etmoore';
 
+// Print out messages
 function printMessage(username, badgeCount, points){
   var message = username + " has " + badgeCount + " total badge(s) and " + points + " JavaScript points.";
   console.log(message);
 }
 
-// Connect  to the API URL (http://teamtreehouse.com/username.json)
+// Print out error messages
+function printError(e){
+  console.log(e.message);
+}
 
+// Connect  to the API URL (http://teamtreehouse.com/username.json)
 var req = http.get('http://teamtreehouse.com/' + username + '.json', function(res){
-  console.log(res.statusCode);
   var body = "";
   // Read the data
   res.on('data', function(chunk){
     body += chunk;
   });
   res.on('end', function () {
-    var profile = JSON.parse(body);
-    printMessage(username, profile.badges.length, profile.points.JavaScript);
+    if (res.statusCode === 200){
+      try {
+        // Parse the data
+        var profile = JSON.parse(body);
+        // Print the data
+        printMessage(username, profile.badges.length, profile.points.JavaScript);
+      } catch(error) {
+        // Parse Error
+        printError(error);
+      }
+    } else {
+      // Status Code Error
+      printError({message: "There was an error getting the profile for " + username + ". (" + http.STATUS_CODES[res.statusCode] + ")"});
+    }
   });
-  // Parse the data
-  // Print the data
+
+
 });
 
-req.on('error', function(e){
-  console.error(e.message);
-});
+// Connection Error
+req.on('error', printError);
